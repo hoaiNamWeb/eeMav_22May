4d85b65c94f2a5d71b9393c2af450a8588b2bec9
hoaiNamWeb <hoainam.programmer@gmail.com>
2023-05-22 11:08:50 +0700

    commit all

---
+++ src/main/java/com/example/eemav_22may/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.example.eemav_22may;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "message Hello Hoài Nam";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("Ngu \u1ec5  Hoài Nam");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%--<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>--%>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - 22May Hoài Nam</title>
+</head>
+<body>
+<h1><%= "Hello Hoài Nam 9!" %>
+</h1>
+<br/>
+<a href="hello-servlet">Hello Nguyễn Hoài Nam link</a>
+</body>
+</html>
